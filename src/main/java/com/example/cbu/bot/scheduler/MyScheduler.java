dd25679a48c25d845333d669e11ca32aa494b107
Aziz1019 <starinc1998@gmail.com>
2022-12-20 17:28:07 +0500

    minor changes

--- src/main/java/com/example/cbu/bot/scheduler/MyScheduler.java
+++ src/main/java/com/example/cbu/bot/scheduler/MyScheduler.java
@@ -1,11 +1,13 @@
 package com.example.cbu.bot.scheduler;
 
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.support.CronTrigger;
 import org.springframework.stereotype.Component;
 
 
 @Component
+@EnableScheduling
 public class MyScheduler {
     private final TaskScheduler executor;
     public MyScheduler(TaskScheduler taskExecutor) {
@@ -13,9 +15,9 @@ public class MyScheduler {
     }
     public void scheduling(final Runnable task, String cron) {
         // Every cron seconds
-//        executor.schedule(task, new CronTrigger("*/" + cron + " * * * * * "));
+        executor.schedule(task, new CronTrigger("*/" + cron + " * * * * * "));
 
         // Daily at cron time
-        executor.schedule(task, new CronTrigger("0 0 " + cron + " * * * "));
+//        executor.schedule(task, new CronTrigger("0 0 " + cron + " * * * "));
     }
 }
